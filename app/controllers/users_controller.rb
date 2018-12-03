c97a1d130c9afa9170cfb91f0a62782f2089aee2
49V <moshelawlor@gmail.com>
2018-12-03 00:54:49 -0500

    Finish User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
-  def new
+
+  def signup
+    @user = User.new
+    render 'signup'
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome #{@user.name}, thanks for signing up!"
+      redirect_to root_path
+    else
+      render 'signup'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
   get '/account',   to: 'static_pages#account'
   get '/candidates', to: 'static_pages#candidates'
   get '/jobs',    to: 'static_pages#jobs'
-  get '/signup',  to: 'users#new'
+  get '/users',  to: 'users#signup'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
