66af7c308f8da38fbabcb98959e6103a9d7b5833
49V <moshelawlor@gmail.com>
2018-12-05 16:35:23 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :require_login
+  before_action :logged_in_user
 
   def home
   end
@@ -12,14 +12,5 @@ class StaticPagesController < ApplicationController
 
   def job
   end
-
-    private
-
-    def require_login
-      unless logged_in?
-        flash[:danger] = "You must be logged in to access this section"
-        redirect_to login_path # halts request cycle
-      end
-    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/candidates', to: 'static_pages#candidates'
   get '/jobs',       to: 'static_pages#jobs'
   get '/users',      to: 'users#signup'
+  get '/index',      to: 'users#index'
   get '/login',      to: 'sessions#new'
   post '/login',     to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
